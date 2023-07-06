60dd98bddbb92c0827c4792bd69f9e1f9c4a8c98
CreateYourReality <timo.moewes@icloud.com>
2023-07-06 14:53:00 +0200

    99%

---
+++ TAG4/main.js
@@ -0,0 +1,57 @@
+import { appendFile,mkdir,writeFile } from 'node:fs/promises';
+
+const fileDir = `./log`;
+const errorDir = `./error`;
+const infoDir = `./info`;
+const warnDir = `./warn`;
+
+const filePath =  new URL(`${fileDir}/log.txt`,import.meta.url);
+const errorPath = new URL(`${errorDir}/error.txt`,import.meta.url);
+const infoPath = new URL(`${infoDir}/info.txt`,import.meta.url);
+const warnPath = new URL(`${warnDir}/warn.txt`,import.meta.url);
+
+const dirArray = [fileDir,errorDir,infoDir,warnDir]
+
+const logSuffix = "\n"
+
+const createPaths = async (array) => {
+    for(let i = 0; i<array.length;i++){
+        try{
+            await mkdir(array[i])
+        }catch{
+            (e) => console.log(e);
+        }
+    }
+}
+
+const logME = async (logTXT, logType) => {
+    const date =  new Date().toUTCString()
+    let output = ``;
+    logType? output = `${logType} :: ${date} :: ${logTXT+logSuffix}` : output = `${date} :: ${logTXT+logSuffix}`
+    
+    try{
+        console.log(logType);
+        switch(logType){
+            case "warn": await appendFile(warnPath,output);break;
+            case "error": await appendFile(errorPath,output);break;
+            case "info": await appendFile(infoPath,output);break;
+            default: break;
+        }
+        await appendFile(filePath,output);
+    }catch{
+        (e) => {console.log(e);}
+    }
+
+    return true;
+
+}
+
+    await createPaths(dirArray);
+    await logME("text","warn").catch((e) => console.log("Here: ",e))
+    await logME("text","warn").catch((e) => console.log("Here: ",e))
+    await logME("text","error").catch((e) => console.log("Here: ",e))
+    await logME("text","info").catch((e) => console.log("Here: ",e))
+    await logME("text","error").catch((e) => console.log("Here: ",e))
+    await logME("text").catch((e) => console.log("Here: ",e))
+
+
